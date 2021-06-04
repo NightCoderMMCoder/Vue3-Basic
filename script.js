075d2723e9da2f8501bbe2ea7a1a7861254da395
aungmyat12 <aungmyatoo2580@gmail.com>
2021-06-04 23:25:22 +0630

    3-Data

---
+++ script.js
@@ -0,0 +1,21 @@
+// const h1 = document.querySelector("h1");
+// const button = document.querySelector("button");
+
+// let message = "Welcome To Vue3 Course";
+// h1.innerText = message;
+
+// button.addEventListener("click", handleClick);
+
+// function handleClick() {
+//   message = "Vue 3 Course";
+//   // h1.innerText = message;
+// }
+
+Vue.createApp({
+  // template, data, methods
+  data() {
+    return {
+      message: "Welcome To Vue3 Course",
+    };
+  },
+}).mount("#app");
